24dff8e857ad105293b692933789cb859e8330f6
Evan Decker <jevandecker@gmail.com>
2025-09-23 14:15:33 -0600

    Made a nubmer guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = 650;
+
+        Random randomGenerator = new Random();
+        int magNumber = randomGenerator.Next(1, 100);
+
+        while (guess != magNumber)
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+
+            if (guess > magNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
