595d7e8b31431f44448f66b50aee50050b8a4646
Evan Decker <jevandecker@gmail.com>
2025-09-23 15:00:51 -0600

    created code to create a numbers list.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int addNumber = -3;
+        int sum = 0;
+        int max = 0;
+        float average;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (addNumber != 0)
+        {
+            Console.WriteLine("Enter number: ");
+            string userNumber = Console.ReadLine();
+            addNumber = int.Parse(userNumber);
+
+            if (addNumber != 0)
+            {
+                numbers.Add(addNumber);
+            }
+        }
+
+        Console.WriteLine(numbers.Count);
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        average = sum / numbers.Count;
+        Console.WriteLine($"The sum is : {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
